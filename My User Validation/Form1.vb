4277cbd025d8c687ea6bc77a7c5518295b9d0dbc
@luisedward777 <luisedward.miranda@gmail.com>
2018-04-03 03:29:08 +0800

    Initial Commit

---
+++ My User Validation/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MaskedTextBox1.Text = "555-55-1212" Then
+            MsgBox("Welcome to the system!")
+        Else
+            MsgBox("I don't recognize this number")
+        End If
+    End Sub
+End Class
